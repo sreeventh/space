3ef2f5ea6474b94a4fc76acf61a9960de02f296f
sreeventh <sreeventhsrinivasan@gmail.com>
2024-03-01 11:16:08 +0530

    y

--- src/components/Astro.jsx
+++ src/components/Astro.jsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 export default function Astro(props) {
+    const [vis, setVis] = useState(false)
+
+    function handleCustomKey(event) {
+        if (event.key === "y") {
+            setVis(true);
+        }
+    }
+    console.log(vis);
+    useEffect(() => {
+        // Add event listeners when the component mounts
+        document.addEventListener("keydown", handleCustomKey);
+
+        // Cleanup function to remove event listeners when the component unmounts
+        return () => {
+            document.removeEventListener("keydown", handleCustomKey);
+        };
+    }, []);
     return (
         <>
-            <input
-                type="text"
-                onChange={props.handleInputChange}
-                onKeyDown={props.handleKeyPress}
-                value={props.Astroname}
-                placeholder="Enter Astronaut"
-                style={{
-                    width: "400px",
-                    height: "60px",
-                    padding: "20px",
-                    marginBottom: "10px",
-                    marginTop: "100px",
-                    marginLeft: "600px",
-                    border: "none",
-                    borderRadius: "5px",
-                    backgroundColor: "#121212", // Darker background color
-                    color: "#ffffff", // White text color
-                    outline: "none",
-                    fontSize: "1.5em",
-                    opacity:"80%"
-                }}
-                required
-            />
+            {vis && (
+
+                <input
+                    type="text"
+                    onChange={props.handleInputChange}
+                    onKeyDown={props.handleKeyPress}
+                    value={props.Astroname}
+                    placeholder="Enter Astronaut"
+                    style={{
+                        width: "400px",
+                        height: "60px",
+                        padding: "20px",
+                        marginBottom: "10px",
+                        marginTop: "100px",
+                        marginLeft: "600px",
+                        border: "none",
+                        borderRadius: "5px",
+                        backgroundColor: "#121212", // Darker background color
+                        color: "#ffffff", // White text color
+                        outline: "none",
+                        fontSize: "1.5em",
+                        opacity: "80%"
+                    }}
+                    required
+                />
+            )}
         </>
     )
 }
